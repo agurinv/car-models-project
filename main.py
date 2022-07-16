d60277821c8b61e7e3817c1d4b2feacd5c8949bb
AlexHuryn <97898297+AlexHuryn@users.noreply.github.com>
2022-07-16 22:07:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 class Car:
-    def __init__(self, length, width, height, wheel)
+    def __init__(self, length, width, height, wheel, year, mileage):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.wheel = wheel
+        self.year = year
+        self.mileage = mileage
+
+    def consumption(self):
+        pass
+
+
+class Engine:
+    def __init__(self, power, cylinder):
+        self.power = power
+        self.cylinder = cylinder
+
+
+class Owner:
+    def __init__(self, name, age, birthday, number):
+        self.name = name
+        self.age = age
+        self.birthday = birthday
+        self.number = number
