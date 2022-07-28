c9def0e961313a004ec83556197e75a394f0c3dd
Alex Huryn <algurin77@gmail.com>
2022-07-28 17:38:58 +0300

    Abstract class and method fix

--- car-inheritance/main.py
+++ car-inheritance/main.py
@@ -1,10 +1,11 @@
 import json
 import os
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 
 class BaseCar(ABC):
-    def __init__(self, length: int, width :int, height: int, wheel: int, year: int,
+    def __init__(self, length: int, width: int, height: int, wheel: int, year: int,
                  mileage: float, power: float, amount_of_cylindres: int, car_engine: int = 0):
         self.length = length
         self.width = width
@@ -35,13 +36,10 @@ class Owner:
 class TrackCar(BaseCar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def wheelCheck(self, wheel):
-        if self.wheel == 0:
-            raise Exception('A car cannot have 0 wheels!')
+        self.wheelCheck = wheel_check_instance.wheel_check_method()
 
     def trailer(self, wheel):
-        #The tractor itself has 4 wheels (Сам тягач имеет 4 колеса)
+        # The tractor itself has 4 wheels (Сам тягач имеет 4 колеса)
         return self.wheel - 4
 
     def engine(self) -> float:
@@ -54,25 +52,29 @@ print(my_car.consumption())
 jsonCar = json.dumps(my_car.__dict__)
 print(jsonCar)
 
-track_car = Track(1, 2, 3, 8, 2006, 6000, 900, 200)
+track_car = TrackCar(1, 2, 3, 8, 2006, 6000, 900, 200)
 
 track_car.carEngine()
 
 track_car.updateEngine()
 
-class PassengerCar(Car):
+
+class PassengerCar(BaseCar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.carEngine = my_car.engine()
+        self.wheelCheck = wheel_check_instance.wheel_check_method()
 
-    def wheelCheckPass(self, wheel):
+    def wheel_check_pass(self):
         if self.wheel == 0:
             raise Exception('A car cannot have 0 wheels!')
 
-    def consumptionPass(self, wheel):
-        return self.wheel * 0,34 * 100
+    @abstractmethod
+    def consumption_pass(self) -> tuple[int, int]:
+        return self.wheel * 0, 34 * 100
 
-    def updatePassEngine(self, carEngine):
+    @abstractmethod
+    def update_pass_engine(self) -> float:
         self.carEngine = self.power // 100 * self.cylinder
         return self.carEngine
 
@@ -82,13 +84,23 @@ print(my_passcar.consumptionPass(8))
 jsonPassCar = json.dumps(my_passcar.__dict__)
 print(jsonPassCar)
 
-#environment variables
+# environment variables
 print("The keys and values of all environment variables:")
 for key in os.environ:
     print(key, "=>", os.environ[key])
 
 print("The value of HOME is: ", os.environ["HOME"])
 
+print("Some changes")
 
 
-print ("Some changes")
+class wheel_check(ABC):
+    def __init__(self):
+        pass
+
+    def wheel_check_method(self):
+        if self.wheel == 0:
+            raise Exception('A car cannot have 0 wheels!')
+
+
+wheel_check_instance = wheel_check()
